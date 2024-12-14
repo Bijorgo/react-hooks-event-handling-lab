2bbb07154bcdc03c2757a579531adfd06d6a88be
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-12-14 15:19:39 -0700

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function focusHandler() {
+        console.log("Good!")
+    }
+    function blurHandler() {
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <div>
+            <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+        </div>
+        
+    )
+}
+
+export default EyesOnMe;
